a0dd6ca18357a6fdf4c759e498e899a6001201e5
akldrix <125759771+akldrix@users.noreply.github.com>
2025-12-23 19:00:18 +0300

    created useCalendar hook

---
+++ src/hooks/useCalendar.jsx
@@ -0,0 +1,35 @@
+
+import { useState } from "react";
+
+export const useCalendar = () => {
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  
+  const monthName = currentDate.toLocaleString("default", { month: "long" });
+  const year = currentDate.getFullYear();
+
+
+  const daysInMonth = new Date(year, currentDate.getMonth() + 1, 0).getDate();
+
+
+  let startDay = new Date(year, currentDate.getMonth(), 1).getDay();
+  if (startDay === 0) startDay = 7;
+
+  const nextMonth = () => {
+    setCurrentDate(new Date(year, currentDate.getMonth() + 1, 1));
+  };
+
+  const prevMonth = () => {
+    setCurrentDate(new Date(year, currentDate.getMonth() - 1, 1));
+  };
+
+  return {
+    currentDate,
+    monthName,
+    year,
+    daysInMonth,
+    startDay, 
+    nextMonth,
+    prevMonth,
+  };
+};
