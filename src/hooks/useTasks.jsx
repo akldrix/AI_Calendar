7cd218fd30a6b4773749858d839d5a264a7e4d88
akldrix <125759771+akldrix@users.noreply.github.com>
2026-03-05 21:39:37 +0300

    tasks bug fixed

--- src/hooks/useTasks.jsx
+++ src/hooks/useTasks.jsx
@@ -5,7 +5,7 @@ import React from "react";
 export const useTasks = () => {
   const queryClient = useQueryClient();
   const {
-    data: tasks,
+    data: tasks = [],
     isLoading,
     error,
   } = useQuery({
